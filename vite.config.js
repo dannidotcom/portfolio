05b2d0c1524e219013b661d24d67c7743f1aba44
dannidotcom <alphonse.danni@gmail.com>
2024-11-19 16:27:15 +0300

    beta

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { ghPages } from 'vite-plugin-gh-pages';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
+  base: '/portfolio',
+  plugins: [react(), ghPages()],
   css: {
     preprocessorOptions: {
       scss: {
